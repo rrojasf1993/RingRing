641d9ac0929f0b5fdcece5004d9996c4efd3999f
rrojas <rrojas@arrivelogistics.com>
2023-08-15 15:17:01 -0500

    First version

---
+++ TestRingLing.Cross/DTO/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TestRingLing.Cross.DTO
+{
+	public class Customer
+	{
+		public Customer()
+		{
+		}
+	}
+}
+
---
+++ TestRingLing.Data/CsvReader.cs
@@ -0,0 +1,6 @@
+﻿namespace TestRingLing.Data;
+public class Class1
+{
+
+}
+
