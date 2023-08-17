11c616940f5e5ef027bbf98cf63b55316d698704
alejo0512 <alejo@live.cn>
2023-08-17 10:18:56 -0500

    Call styles

--- TestRingLing/ClientApp/src/components/IncomingCall.js
+++ TestRingLing/ClientApp/src/components/IncomingCall.js
@@ -7,42 +7,46 @@ const IncomingCall=()=>{
             width: '18rem'
         }}
     >
-        <img
+        <div style={{'width':'100%', 'margin-top': '15%'}}>
+        <img style={{'border-radius': '150px', 'width': '200px'}}
             alt="Sample"
+            className="custom-center "
             src="https://picsum.photos/200"
         />
+        </div>
+        
         <CardBody>
-            <CardTitle tag="h5">
+            <CardTitle tag="h5" style={{'text-align': 'center','margin-top': '20px'}}>
                John Doe
     </CardTitle>
-            <CardSubtitle
+            <CardSubtitle style={{'text-align': 'center'}}
                 className="mb-2 text-muted"
                 tag="h6"
             >
                 Lead Cargo Tracker
                 
     </CardSubtitle>
-            <CardText>
+            <CardText style={{'text-align': 'center','margin-bottom': '40px'}}>
                 <b> Coca Cola Inc</b>
     </CardText>
             <div className="row">
                 <div className="col-md-4">
-                    <Button color ="info">Forward</Button>
+                    <Button color ="info" className="custom-rounded-button">Forward</Button>
                 </div>
                 <div className="col-md-4">
-                    <Button color="secondary">Reply</Button>
+                    <Button color="secondary" className="custom-rounded-button">Reply</Button>
                 </div>
                 <div className="col-md-4">
-                    <Button color="danger">Ignore</Button>
+                    <Button color="danger" className="custom-rounded-button">Ignore</Button>
                 </div>
 
             </div>
             <div className="row">
                 <div className="col-md-6">
-                    <Button color="warning">Voice Mail</Button>
+                    <Button color="warning" className="custom-rounded-button custom-float-right">Voice Mail</Button>
                 </div>
                 <div className="col-md-6">
-                    <Button color="success">Answer</Button>
+                    <Button color="success" className="custom-rounded-button">Answer</Button>
                 </div>
 
             </div>
